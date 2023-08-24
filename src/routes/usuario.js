0b47f78b93c6c9140f1366cee93cc915b9de7287
Arthur Porto <artfporto@gmail.com>
2023-08-24 09:07:12 -0300

    auth

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -8,6 +8,8 @@ import {
   updateUsuario,
 } from "../db/index.js";
 
+import verificarAutenticacao from "../middlewares/autenticacao.js";
+
 const router = Router();
 
 router.get("/usuario", async (req, res) => {
@@ -31,7 +33,7 @@ router.get("/usuario/:id", async (req, res) => {
   }
 });
 
-router.post("/usuario", async (req, res) => {
+router.post("/usuario", verificarAutenticacao, async (req, res) => {
   console.log("Rota POST /usuario solicitada");
   try {
     await insertUsuario(req.body);
@@ -41,7 +43,7 @@ router.post("/usuario", async (req, res) => {
   }
 });
 
-router.put("/usuario", async (req, res) => {
+router.put("/usuario", verificarAutenticacao, async (req, res) => {
   console.log("Rota PUT /usuario solicitada");
   try {
     const usuario = await selectUsuario(req.body.id);
@@ -55,7 +57,7 @@ router.put("/usuario", async (req, res) => {
   }
 });
 
-router.delete("/usuario/:id", async (req, res) => {
+router.delete("/usuario/:id", verificarAutenticacao, async (req, res) => {
   console.log("Rota DELETE /usuario solicitada");
   try {
     await deleteUsuario(req.params.id);
